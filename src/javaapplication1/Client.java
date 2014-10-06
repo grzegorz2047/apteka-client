17547db62dfc9ac9441fc5f46ca6534c7b998b91
student_U487 <student_U487@V13B.edu.st.poznan.pl>
2014-10-06 12:45:19 +0200

    dodanie template

--- src/javaapplication1/Client.java
+++ src/javaapplication1/Client.java
@@ -74,54 +74,5 @@ public class Client  {
   public static void main(String[] args) {
     MysqlHandler.PrzypiszDane("127.0.0.1", "3306", "root", "");
     createMenu();
-    /*String[] labels = { "Imie", "Nazwisko", "Tytul" };
-    char[] mnemonics = { 'F', 'L', 'A' };
-    int[] widths = { 15, 15, 14 };
-    String[] descs = { "First Name", "Last Name", "Tytul" };
-
-    final TextForm form = new TextForm(labels, mnemonics, widths, descs);
-
-    JButton submit = new JButton("Wyslij zapytanie");
-
-    submit.addActionListener(new ActionListener() {
-        @Override
-      public void actionPerformed(ActionEvent e) {
-        System.out.println(form.getText(0) + " " + form.getText(1) + ". " + form.getText(2)
-           );
-        if(form.getText(1).isEmpty() || form.getText(0).isEmpty()){
-            JOptionPane.showMessageDialog(form,
-            "uzupelnij pola!",
-            "Nieuzupelnione pola",
-            JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-        if(!form.getText(2).equals("mgr") && !form.getText(2).equals("tech")){
-            JOptionPane.showMessageDialog(form,
-            "Tytul moze byc tylko tech albo mgr!",
-            "Zla nazwa w polu tytul",
-            JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-            try {
-                MysqlHandler.dodajRekordPracownik(form.getText(0),form.getText(1), form.getText(2));
-                JOptionPane.showMessageDialog(form,
-                "Wprowadzono rekord do bazy!",
-                "Informacja",
-                JOptionPane.INFORMATION_MESSAGE);
-            } catch (Exception ex) {
-                System.out.println("Blad przy dodawaniu pracownika!");
-                ex.printStackTrace();
-            }
-      }
-    });
-
-    JFrame f = new JFrame("SQL client");
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.getContentPane().add(form, BorderLayout.NORTH);
-    JPanel p = new JPanel();
-    p.add(submit);
-    f.getContentPane().add(p, BorderLayout.SOUTH);
-    f.pack();
-    f.setVisible(true);*/
   }
 }
--- src/javaapplication1/InsertDataForm.java
+++ src/javaapplication1/InsertDataForm.java
@@ -4,7 +4,13 @@
  */
 package javaapplication1;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  *
@@ -18,7 +24,46 @@ public class InsertDataForm {
         
         JButton submit = new JButton("Wyslij zapytanie");
 
-        
+        submit.addActionListener(new ActionListener() {
+            @Override
+          public void actionPerformed(ActionEvent e) {
+            System.out.println(form.getText(0) + " " + form.getText(1) + ". " + form.getText(2)
+               );
+            if(form.getText(1).isEmpty() || form.getText(0).isEmpty()){
+                JOptionPane.showMessageDialog(form,
+                "uzupelnij pola!",
+                "Nieuzupelnione pola",
+                JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            if(!form.getText(2).equals("mgr") && !form.getText(2).equals("tech")){
+                JOptionPane.showMessageDialog(form,
+                "Tytul moze byc tylko tech albo mgr!",
+                "Zla nazwa w polu tytul",
+                JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+                try {
+                    MysqlHandler.dodajRekordPracownik(form.getText(0),form.getText(1), form.getText(2));
+                    JOptionPane.showMessageDialog(form,
+                    "Wprowadzono rekord do bazy!",
+                    "Informacja",
+                    JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception ex) {
+                    System.out.println("Blad przy dodawaniu pracownika!");
+                    ex.printStackTrace();
+                }
+          }
+        });
+
+        JFrame f = new JFrame("SQL client");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(form, BorderLayout.NORTH);
+        JPanel p = new JPanel();
+        p.add(submit);
+        f.getContentPane().add(p, BorderLayout.SOUTH);
+        f.pack();
+        f.setVisible(true);
     }
     
 }
